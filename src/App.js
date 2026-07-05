a2e3ab6b78b2b2957d731e1cf98570c8a953ebeb
unknown <thbusae@gmail.com>
2026-07-05 15:53:10 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const name = "리액트";
+  return (
+    <>
+      {/* 주석 */}
+      <div className="react">{name}</div>
+      <input />
+    </>
+  );
+}
+
+export default App;
